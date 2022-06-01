e6547d1f1a4a432bc4324cd894a10fffb8b3b985
Prince Thind <princethind799@gmail.com>
2022-06-01 16:14:39 +0530

    bound ball velocity

--- src/components/ball.js
+++ src/components/ball.js
@@ -6,7 +6,7 @@ const ball = {
   radius: windowHeight / 50,
   velocity: {
     x: windowWidth / 300,
-    y: windowHeight / 300,
+    y: windowWidth / 300,
   },
 };
 
@@ -15,13 +15,14 @@ function drawBall() {
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
   ctx.fill();
   checkCollisionsWithBorders();
+  boundVelocities();
   ball.x += ball.velocity.x;
   ball.y += ball.velocity.y;
 }
 
 function checkCollisionsWithBorders() {
-  const randomNoise = Math.random()*(1.2-0.9)+0.9;
-  
+  const randomNoise = Math.random() * (1.2 - 0.9) + 0.9;
+
   const ballRight = ball.x + ball.radius;
   const ballLeft = ball.x - ball.radius;
   const ballTop = ball.y - ball.radius;
@@ -29,20 +30,29 @@ function checkCollisionsWithBorders() {
 
   if (ballLeft <= 0) {
     ball.x = ball.radius;
-    ball.velocity.x = -ball.velocity.x*randomNoise;
+    ball.velocity.x = -ball.velocity.x * randomNoise;
   }
   if (ballRight >= windowWidth) {
     ball.x = windowWidth - ball.radius;
-    ball.velocity.x = -ball.velocity.x*randomNoise;
+    ball.velocity.x = -ball.velocity.x * randomNoise;
   }
 
   if (ballTop <= 0) {
     ball.y = ball.radius;
-    ball.velocity.y = -ball.velocity.y*randomNoise;
+    ball.velocity.y = -ball.velocity.y * randomNoise;
   }
   if (ballBottom >= windowHeight) {
     ball.y = windowHeight - ball.radius;
-    ball.velocity.y = -ball.velocity.y*randomNoise;
+    ball.velocity.y = -ball.velocity.y * randomNoise;
+  }
+}
+
+function boundVelocities() {
+  if (ball.velocity.x > windowWidth / 50) {
+    ball.velocity.x = windowWidth / 50;
+  }
+  if (ball.velocity.y > windowWidth / 50) {
+    ball.velocity.y = windowWidth / 50;
   }
 }
 

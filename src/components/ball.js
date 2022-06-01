47908c566f2806149be9e864445455156543cc4f
Prince Thind <princethind799@gmail.com>
2022-06-01 16:07:01 +0530

    implement basic ball movement

--- src/components/ball.js
+++ src/components/ball.js
@@ -3,21 +3,47 @@ import { ctx, windowHeight, windowWidth } from "./canvas";
 const ball = {
   x: windowWidth / 2,
   y: windowHeight / 2,
-  radius:5,
+  radius: windowHeight / 50,
   velocity: {
-    x: 3,
-    y: 3,
+    x: windowWidth / 300,
+    y: windowHeight / 300,
   },
 };
 
-
 function drawBall() {
   ctx.beginPath();
-//   ctx.moveTo(ball.x, ball.y);
-  ctx.arc(ball.x,ball.y,ball.radius,0, Math.PI*2);
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
   ctx.fill();
-  ball.x+=2;
-  ball.y+=2;
+  checkCollisionsWithBorders();
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
 }
 
-export  { ball, drawBall };
+function checkCollisionsWithBorders() {
+  const randomNoise = Math.random()*(1.2-0.9)+0.9;
+  
+  const ballRight = ball.x + ball.radius;
+  const ballLeft = ball.x - ball.radius;
+  const ballTop = ball.y - ball.radius;
+  const ballBottom = ball.y + ball.radius;
+
+  if (ballLeft <= 0) {
+    ball.x = ball.radius;
+    ball.velocity.x = -ball.velocity.x*randomNoise;
+  }
+  if (ballRight >= windowWidth) {
+    ball.x = windowWidth - ball.radius;
+    ball.velocity.x = -ball.velocity.x*randomNoise;
+  }
+
+  if (ballTop <= 0) {
+    ball.y = ball.radius;
+    ball.velocity.y = -ball.velocity.y*randomNoise;
+  }
+  if (ballBottom >= windowHeight) {
+    ball.y = windowHeight - ball.radius;
+    ball.velocity.y = -ball.velocity.y*randomNoise;
+  }
+}
+
+export { ball, drawBall };

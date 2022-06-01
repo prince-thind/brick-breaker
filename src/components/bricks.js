da172ac632a5a097d093306c91ab0cfe128f0d3b
Prince Thind <princethind799@gmail.com>
2022-06-01 18:57:02 +0530

    make bricks

---
+++ src/components/bricks.js
@@ -0,0 +1,37 @@
+import { ctx, windowHeight, windowWidth } from "./canvas";
+
+const bricks = [];
+
+const bricksPerRow = 10;
+const numberOfColumns = 3;
+
+const divisionsX = bricksPerRow + 1;
+const divisionsY = numberOfColumns + 1;
+
+const horizontalSlot = windowWidth / divisionsX;
+const verticalSlot = windowHeight / 4 / divisionsY;
+
+const verticalOffset=windowHeight/20;
+
+const brickWidth = horizontalSlot * 0.9;
+const brickHeight = verticalSlot * 0.75;
+
+for (let i = 0; i < numberOfColumns; i++) {
+  for (let j = 0; j < bricksPerRow; j++) {
+    const brick = {};
+    const xCordinate = horizontalSlot * j + horizontalSlot / 2;
+    const yCordinate = verticalSlot * i + verticalSlot / 2 +verticalOffset;
+    brick.x = xCordinate;
+    brick.y = yCordinate;
+    brick.hit = false;
+    bricks.push(brick);
+  }
+}
+
+function drawBricks() {
+  for (const brick of bricks) {
+    ctx.fillRect(brick.x, brick.y, brickWidth, brickHeight);
+  }
+}
+
+export { drawBricks };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import { drawBall } from "./components/ball";
 import { clearCanvas } from "./components/canvas";
+import { drawBricks } from "./components/bricks";
 
 function animate() {
   clearCanvas();
   drawBall();
+  drawBricks()
   requestAnimationFrame(animate);
 }
 
